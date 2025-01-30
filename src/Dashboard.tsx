bdaf9610e6bf23a9df106e040bdee709380fb642
Eric Schramm <eschramm@mac.com>
2025-01-30 09:07:32 -0600

    add more logging

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -25,6 +25,7 @@ export default function Dashboard() {
         if (window.webkit?.messageHandlers.ScanCode) {
 			console.log('inside ScanCode check');
             window.webkit?.messageHandlers.ScanCode.postMessage('');
+			console.log('after call to ScanCode.postMessage');
         }
     }
 
