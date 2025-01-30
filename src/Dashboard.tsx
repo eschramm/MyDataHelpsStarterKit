eee6a35dc575d0582600b67c1d5f6c691e81b9f7
Eric Schramm <eschramm@mac.com>
2025-01-30 08:37:53 -0600

    try ScanCode

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -9,14 +9,27 @@ export default function Dashboard() {
 		MyDataHelps.startSurvey(surveyName);
 	}
 
+	function scanCode() {
+		console.log('ScanCode tapped');
+        if (window.webkit.messageHandlers.ScanCode) {
+			console.log('inside ScanCode check');
+            window.webkit.messageHandlers.ScanCode.postMessage('');
+        }
+    }
+
 	return (
 		<div className='dashboard-container'>
 		<Layout colorScheme="auto">
 			<Card>
 				<div className='card-innards'>
 					<h1>Testing Stuff</h1>
-					<button onClick={() => { openSurvey('Full Battery Test'); }}>
-						Open Survey example
+					
+					<button className='cta-button' onClick={() => { openSurvey('Full Battery Test'); }}>
+						<div className='cta-button-title'>Open Survey example</div>
+					</button>
+
+					<button className='cta-button' onClick={() => { scanCode(); }}>
+						<div className='cta-button-title'>Open Scan Code</div>
 					</button>
 				</div>
 			</Card>
