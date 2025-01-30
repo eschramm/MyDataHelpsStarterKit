173bcc1caa4c3811fd831203514e2640c5ab8145
Eric Schramm <eschramm@mac.com>
2025-01-30 08:22:28 -0600

    remodel - add open survey action

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,8 +1,26 @@
-import DashboardContainer from './DashboardContainer';
+import { Layout, Card } from '@careevolution/mydatahelps-ui';
+import "./Dashboard.css"
+import MyDataHelps from "@careevolution/mydatahelps-js";
+
 
 export default function Dashboard() {
     
+	function openSurvey(surveyName: string) {
+		MyDataHelps.startSurvey(surveyName);
+	}
+
 	return (
-		<DashboardContainer />
+		<div className='dashboard-container'>
+		<Layout colorScheme="auto">
+			<Card>
+				<div className='card-innards'>
+					<h1>Testing Stuff</h1>
+					<button onClick={() => { openSurvey('Full Battery Test'); }}>
+						Open Survey example
+					</button>
+				</div>
+			</Card>
+		</Layout>
+	</div>
 	);
 }
